d4b7d3531cfb9d8a01040be1a90177873922d84e
olcay-elt <olcay.elt@gmail.com>
2023-04-15 00:29:06 +0200

    Add

--- s02/src/app.ts
+++ s02/src/app.ts
@@ -25,7 +25,13 @@ console.log('hello')
 //     }
 // }
 
+// class Employee extends Person{
+//     emplyCode: number;
 
+//     constructor(emplyCode: number, name: string){
+//         super(name);
+//         this.emplyCode = emplyCode;
+//     }
 
 //     display():void{
 //         console.log(` Name: ${this.name}  - code ${this.emplyCode}`)
